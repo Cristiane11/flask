1d75a72ada0f7e5bdbf8325f2d40b369df2fa72b
Cristiane Abreu <bjwmcd@yahoo.com>
2025-10-01 21:08:03 -0400

    update

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask,request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey, Table, Column, String, Integer
+from sqlalchemy import ForeignKey, Table, Column, String, Integer,select
 from marshmallow import ValidationError
 from typing import List, Optional
 
@@ -53,7 +53,120 @@ class Pet(Base):
 
     # One-to-Many
     owners: Mapped[List["User"]] = relationship(secondary=user_pet, back_populates="pets")
+#=======Schemas ====================
+# User Schema
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        
+# Pet Schema
+class PetSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Pet
 
+# Initialize Schemas
+user_schema = UserSchema()
+users_schema = UserSchema(many=True) #Can serialize many User objects (a list of them)
+pet_schema = PetSchema()
+pets_schema = PetSchema(many=True)
+
+#=============Routes============
+#CREATEa User
+@app.route('/users', methods=['POST'])
+def create_user():
+    try:
+        user_data = user_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    new_user = User(name=user_data['name'], email=user_data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+
+    return user_schema.jsonify(new_user), 201  
+#READ USERS
+@app.route('/users', methods=['GET'])
+def get_users():
+    query =select(User)
+    users = db.session.execute(query).scalars().all()
+    return users_schema.jsonify(users), 200
+#READ individual User
+@app.route('/users/<int:id>', methods=['GET']) 
+def get_user(id):
+    user = db.session.get(User,id)
+    return user_schema.jsonify(user), 200
+
+#UPDATE individual User
+@app.route('/users/<int:id>', methods=['PUT'])
+def update_user(id):
+    user = db.session.get(User,id)
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+    
+    try:
+        user_data = user_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    if 'name' in user_data:
+        user.name = user_data['name']
+    if 'email' in user_data:
+        user.email = user_data['email']
+    
+    db.session.commit()
+   
+    return user_schema.jsonify(user), 200 
+   
+#DELETE USER
+@app.route('/users/<int:id>', methods=['DELETE'])    
+def delete_user(id):
+    user = db.session.get(User,id)
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+    
+    db.session.delete(user) 
+    db.session.commit()
+   
+    return jsonify({"Message": f"Succefully User deleted {id} user"}),200
+#CREATE Pet
+@app.route('/pets', methods=['POST'])    
+def create_pet():
+    try:
+        pet_data = pet_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    new_pet = Pet(name=pet_data['name'], animal=pet_data['animal'])
+    db.session.add(new_pet)
+    db.session.commit()
+
+    return pet_schema.jsonify(new_pet), 201
+#ADD pet to User
+@app.route('/users/<int:user_id>/add_pet/<int:pet_id>', methods=['GET'])
+def adopt_pet(user_id, pet_id):
+    user = db.session.get(User,user_id)
+    pet = db.session.get(Pet,pet_id)
+
+    
+    user.pets.append(pet)
+    db.session.commit()
+
+    return user_schema.jsonify({"pemessage": f"User {user.name} adopted {pet.animal}, {pet.name}"}), 200
+
+#Add multiple Pets
+@app.route('/users/<int:user_id>/add_pets', methods=['POST'])   
+def add_pets(user_id):
+    user = db.session.get(User,user_id)
+    
+
+    pet_data = request.json
+    
+    for id in pet_data['pet_ids']:
+        pet = db.session.get(Pet, id)
+        user.pets.append(pet)
+        db.session.commit()
+
+    return jsonify({"message": f"All Pets added"}), 200
 
 if __name__ == "__main__":
     with app.app_context():
